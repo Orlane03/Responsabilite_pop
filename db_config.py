7a9740feb2640cc6b6cf63220d713d7e45dc0aa2
Orlane03 <sonkengorlane@gmail.com>
2025-09-12 00:56:41 +0700

    modification

---
+++ db_config.py
@@ -0,0 +1,19 @@
+# db_config.py
+
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Remplace par tes infos de connexion PostgreSQL
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+def get_engine():
+    url = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(url)
+    return engine
